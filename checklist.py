3b15015caafacfc5dc35b68bc194e8b7fd56fc47
Jack <jackkatzer@protonmail.com>
2019-09-03 09:30:55 -0700

    created file

---
+++ checklist.py
@@ -0,0 +1,53 @@
+checklist=[]
+running=True
+
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    return checklist[index]
+
+def update(index, item):
+    checklist[index] = item
+
+def destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    for list_item in checklist:
+        print(list_item)
+
+def test():
+    create("item1")
+    create("item2")
+    read(1)
+    update(1,"item3")
+    print(read(1))
+    destroy(0)
+    print(checklist)
+    create("item17")
+    list_all_items()
+
+while running:
+    selection=input("Create=C, Read=R, Update=U, Destroy=D, List=L")
+    if selection=="C":
+        addition=input("Input new item.")
+        create(addition)
+        print("Added!")
+    elif selection=="R":
+        index="Enter the index of the item you want to read, with the first item being 0."
+        read(index)
+        print("Removed!")
+    elif selection=="U":
+        index="Enter the index of the item you want to update, with the first item being 0."
+        replacement="What do you want to replace it with?"
+        update(index,replacement)
+        print("Updated!")
+    elif selection=="D":
+        index="Enter the index of the item you want to destroy, with the first item being 0."
+        destroy(index)
+        print("Destroyed!")
+    elif selection=="L":
+        list_all_items()
+    else:
+        print("Invalid command.")
